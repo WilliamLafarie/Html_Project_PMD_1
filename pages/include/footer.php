7920b660631d1ce53f0efad564803f8e7a39e877
Melococoo <melody.akarawitage@hotmail.fr>
2018-11-28 16:56:29 +0100

    footer

--- pages/include/footer.php
+++ pages/include/footer.php
@@ -1,24 +1,27 @@
 <footer>
-    <div id="footer1">
-        <div id="Xavier_1">
-    <img src="./../assets/img/imagefooter.png"> Xavier Caïtucoli
-    PDG de Direct Energie 
-    "Choisir le cyclisme à travers cette équipe chère aux français, est pour nous une évidence..."
+    <div>
+        <div id="footer1">
+            <div id="Xavier_1">
+            <img src="./../assets/img/imagefooter.png"> 
+                <div class="texte_case1">
+                    <h4>Xavier Caïtucoli</h4>
+                    <span class="PDG">PDG de Direct Energie</span>
+                    <p>"Choisir le cyclisme à travers cette équipe chère aux français, est pour nous une évidence..."</p>
+                </div>
+            </div>
+            <div id="Jean_Rene">
+            <img src="./../assets/img/imagefooter2.png"> 
+            <div class="texte_case2">
+            <h4>Jean-René Bernaudeau</h4>
+            <span>Manager de Direct Energie Teams</span>
+            <p>"J'ai choisi Direct Energie car je voulais un partenaire qui soit séduit par le travail de fond que je..."</p>
+            </div>
+            </div>
+        </div>
     </div>
-    <div id="Jean_Rene">
-    <img src="./../assets/img/imagefooter2.png"> Jean-René Bernaudeau
-    Manager de Direct Energie Team
-    "J'ai choisi"
-
-    </div>
-
-
-    </div>
-
-
-<h4>NEWSLETTER
+NEWSLETTER
 RESEAUX SOCIAUX
-KIT DE PRESSE</h4>
+KIT DE PRESSE
 </footer>
 
 </body>
