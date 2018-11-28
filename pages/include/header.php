56d184478b1e6f3f4cefd9c27b528a4d0e926de6
sebastiencosneau <sebastien.cosneau@outlook.com>
2018-11-28 15:21:28 +0100

    test

--- pages/include/header.php
+++ pages/include/header.php
@@ -4,10 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./assets/css/include/header.css">
-    <link rel="stylesheet" href="./assets/css/include/footer.css">
+    <link rel="stylesheet" href="./../assets/css/include/header.css">
+    <link rel="stylesheet" href="./../assets/css/include/footer.css">
     <!-- On charge le style CSS de la page concernée -->
     <link rel="stylesheet" href="./../assets/css/<?php echo $pageNameSplit[0]?>.css">
     <title><?php echo $pageNameSplit[0]?></title>
 </head>
 <body>
+    <header>
+        <nav class="containers">
+            <a href="#"><img src="./../assets/img/slogan.png" alt="logo"/></a>
+            <a href="#"><img src="./../assets/img/Home/homelogo.png" alt="home"></a>
+            <a href="#"><i class=""></i>Actualités</a>
+            <a href="#"><i class=""></i>Hors-course</a>
+            <a href="#"><i class=""></i>Calendrier</a>
+            <a href="#"><i class=""></i>L'équipe 2016</a>
+            <a href="#"><i class=""></i>Galerie</a>
+            <a href="#"><i class=""></i>Social Live</a>
+            <a href="#"><i class=""></i>Partenaires</a>
+        </nav>
+    </header>
