add175f64c65ca1d34ca033020308fd4645e9452
WilliamLafarie <45195777+WilliamLafarie@users.noreply.github.com>
2018-11-28 15:32:41 +0100

    Update header.php

--- pages/include/header.php
+++ pages/include/header.php
@@ -4,16 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<<<<<<< HEAD
-    <link rel="stylesheet" href="./../assets/css/include/header.css">
-    <link rel="stylesheet" href="./../assets/css/include/footer.css">
-=======
-
     <link rel="stylesheet" href="./../assets/css/reset.css">
     <link rel="stylesheet" href="./../assets/css/include/header.css">
     <link rel="stylesheet" href="./../assets/css/include/footer.css">
     
->>>>>>> 372335f4745cdebeefd6b8065a5514f920597ffb
     <!-- On charge le style CSS de la page concernée -->
     <link rel="stylesheet" href="./../assets/css/<?php echo $pageNameSplit[0]?>.css">
     <title><?php echo $pageNameSplit[0]?></title>
