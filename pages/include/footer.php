ef1a179c56f27cd416fa5b5d1083655698150fcf
Melococoo <melody.akarawitage@hotmail.fr>
2018-11-28 15:21:45 +0100

    testmelo

--- pages/include/footer.php
+++ pages/include/footer.php
@@ -1,5 +1,24 @@
 <footer>
+    <div id="footer1">
+        <div id="Xavier_1">
+    <img src="./../assets/img/imagefooter.png"> Xavier Caïtucoli
+    PDG de Direct Energie 
+    "Choisir le cyclisme à travers cette équipe chère aux français, est pour nous une évidence..."
+    </div>
+    <div id="Jean_Rene">
+    <img src="./../assets/img/imagefooter2.png"> Jean-René Bernaudeau
+    Manager de Direct Energie Team
+    "J'ai choisi"
 
+    </div>
+
+
+    </div>
+
+
+<h4>NEWSLETTER
+RESEAUX SOCIAUX
+KIT DE PRESSE</h4>
 </footer>
 
 </body>
