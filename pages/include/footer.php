6ad5e8bb9f1cb6c56bbb13e172e61e7a21576cdf
Melococoo <melody.akarawitage@hotmail.fr>
2018-11-29 15:08:23 +0100

    footer changes 15h08

--- pages/include/footer.php
+++ pages/include/footer.php
@@ -1,43 +1,49 @@
 <footer>
-        <section id="footer1">
-                <div class="content2">
-                        <div id="Xavier_1">
-                                <img src="./../assets/img/imagefooter.png" class="imagefoot">
-                                <article class="art_1">
-                                        <h4>Xavier Caïtucoli</h4>
-                                        <span class="PDG">PDG de Direct Energie</span>
-                                        <p class="para"> &laquo; Choisir le cyclisme à travers cette équipe chère aux
-                                                français, est
-                                                pour nous une évidence...</p>
-                                        <button class="lire_suite" type="button">lire la suite
-                                        </button>
-                                </article>
+        <div class="body">
+                <section id="footer1">
+                        <div class="content2">
+                                <div id="Xavier_1">
+                                        <img src="./../assets/img/imagefooter.png" class="imagefoot">
+                                        <article class="art_1">
+                                                <h4>Xavier Caïtucoli</h4>
+                                                <span class="PDG">PDG de Direct Energie</span>
+                                                <p class="para"> &laquo; Choisir le cyclisme à travers cette équipe
+                                                        chère aux
+                                                        français, est
+                                                        pour nous une évidence...</p>
+                                                <button class="lire_suite" type="button">lire la suite
+                                                </button>
+                                        </article>
+                                </div>
+
+                                <div id="Xavier_1">
+                                        <img src="./../assets/img/imagefooter2.png" class="imagefoot">
+                                        <article class="art_1">
+                                                <h4>Jean-René Bernaudeau</h4>
+                                                <span class="PDG">Manager de Direct Energie Teams</span>
+                                                <p class="para">&laquo; J'ai choisi Direct Energie car je voulais un
+                                                        partenaire
+                                                        qui
+                                                        soit
+                                                        séduit par le travail de fond que je...</p>
+                                                <button class="lire_suite" type="button">lire la suite
+                                                </button>
+                                        </article>
+                                </div>
                         </div>
 
-                        <div id="Xavier_1">
-                                <img src="./../assets/img/imagefooter2.png" class="imagefoot">
-                                <article class="art_1">
-                                        <h4>Jean-René Bernaudeau</h4>
-                                        <span class="PDG">Manager de Direct Energie Teams</span>
-                                        <p class="para">&laquo; J'ai choisi Direct Energie car je voulais un partenaire
-                                                qui
-                                                soit
-                                                séduit par le travail de fond que je...</p>
-                                        <button class="lire_suite" type="button">lire la suite
-                                        </button>
-                                </article>
+                </section>
+
+                <section id="footer2">
+                        <div class="content3">
+                                <h2>
+                                        <p class="footnew">NEWSLETTER </p>
+                                        <p class="footres"> RESEAUX SOCIAUX </p>
+                                        <p class="footkit">KIT DE PRESSE </p>
+                                </h2>
                         </div>
-                </div>
-
-        </section>
-
-        <section id="footer2">
-                <div class="content3">
-                        <h2> <span class="foot">NEWSLETTER </span>
-                                <span class="foot"> RESEAUX SOCIAUX </span>
-                                <span class="foot">KIT DE PRESSE </span> </h2>
-                </div>
-        </section>
+                </section>
+                <div>
 </footer>
 
 </body>
