a0e19a16a357bfab43f43efe89f21e166a597f88
William-L79 <willou9979@gmail.com>
2018-11-29 23:29:54 +0100

    UPD

--- pages/include/footer.php
+++ pages/include/footer.php
@@ -14,9 +14,9 @@
 
 <footer>
         <!-- -------------------------------- Premier footer ---------------------------------------- -->
-        <div class="body">
+
                 <section id="footer1">
-                        <div class="content_footer1">
+                        <div class="content_footer">
                                 <div id="Xavier_1">
                                         <img src="./../assets/img/imagefooter.png" class="imagefoot">
                                         <article class="art_1">
@@ -52,21 +52,69 @@
 <!-- ------------------------------------- Deuxième footer ------------------------------------------- -->
 
                 <section id="footer2">
-                        <div class="newsletter">
-                                <h2>NEWSLETTER</h2>
-                                <p class="news"> Recevez par mail les derniers résultats, les prochaines courses et les interviews de la Direct Energie Team</p>
+
+                        <div class="content_footer">
+
+                                <section>
+                                        <h2>Newsletter</h2>
+
+                                        <p>Recevez par mail les derniersrésultats, les prochaines courses et les interviews de la Direct Energie Team</p>
+
+                                        <div class="bouton">
+                                                <input type="email" placeholder="votre email">
+                                                <input type="submit" value="Ok">
+                                        </div>
+                                        
+                                </section>
+
+                                <section>
+                                        <h2>Réseaux sociaux</h2>
+
+                                        <figure>
+                                                <img src="" alt="youtube">
+                                                <figcaption>Youtube</figcaption>
+                                        </figure>
+
+                                        <figure>
+                                                <img src="" alt="Twitter">
+                                                <figcaption>Twitter</figcaption>
+                                        </figure>
+
+                                        <figure>
+                                                <img src="" alt="Instagram">
+                                                <figcaption>Instagram</figcaption>
+                                        </figure>
+
+                                        <figure>
+                                                <img src="" alt="Facebook">
+                                                <figcaption>Facebook</figcaption>
+                                        </figure>
+
+                                        <figure>
+                                                <img src="" alt="Google+">
+                                                <figcaption>Google+</figcaption>
+                                        </figure>
+
+                                </section>
+
+                                <section>
+                                        <h2>Kit de presse</h2>
+                                        <p>Télécharger le dossier de presse: Photo des joueurs, communiqué de presse...</p>
+
+                                        <div class="pdf">
+                                                <img src="" alt="pdf">
+                                                <a href="">Télécharger le dossier (33mo)</a>
+                                        </div>
+                                </section>
+
                         </div>
-                        <div class="res">
-                                <h2>RESEAUX SOCIAUX</h2>
-                        </div>
-                        <div class="kit">
-                                <h2>KIT DE PRESSE</h2>
-                                <p class="kit"> Téléchargez le dossier de presse : Photos des joueurs communiqué de presse... </p>
-                        </div>
-                                
-                        </div>
+
+                        <p class="mentions">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        </p>
+
                 </section>
-                <div>
+
 
 </footer>
 
