2e9142f01bd4754d936e287238b49accc9e79eb3
sebastiencosneau <sebastien.cosneau@outlook.com>
2018-11-30 11:54:14 +0100

    update 30/11 11h54 page hors course terminé

--- pages/horsCourse.php
+++ pages/horsCourse.php
@@ -12,58 +12,106 @@
 
 <main>
 
-    <div class="content">
+    <div class="contentglobal">
+        <div class="content">
         <h2>HORS-COURSE</h2>
-        <section class="firstline">
+            <section class="firstline">
 
-            <figure class="iconelineone">
-                <img src="./../assets/img/home/image11accueil.png" alt="micro">
-                <figcaption>
-                    <p>Indiscrétion</p>
-                </figcaption>
-            </figure>
+                <figure class="iconelineone">
+                    <img src="./../assets/img/home/image11accueil.png" alt="micro">
+                    <figcaption>
+                        <p>Indiscrétion</p>
+                    </figcaption>
+                </figure>
 
-            <figure>
-                <img src="./../assets/img/home/image6accueil.png" alt="cycliste">
-                <figcaption>
-                    <p>Lorem ipsum dolor sit amet conse...</p>
-                </figcaption>
-            </figure>
+                <figure>
+                    <img src="./../assets/img/horsCourse/image1horscourses.png" alt="cycliste">
+                    <figcaption>
+                        <p>Lorem ipsum dolor sit amet conse...</p>
+                    </figcaption>
+                </figure>
 
-            <figure>
-                <img src="./../assets/img/home/image6accueil.png" alt="cycliste">
-                <figcaption>
-                    <p>Lorem ipsum dolor sit amet conse...</p>
-                </figcaption>
-            </figure>
+                <figure>
+                    <img src="./../assets/img/horsCourse/image2horscourses.png" alt="cycliste">
+                    <figcaption>
+                        <p>Lorem ipsum dolor sit amet conse...</p>
+                    </figcaption>
+                </figure>
 
-            <figure>
-                <img src="./../assets/img/home/image6accueil.png" alt="cycliste">
-                <figcaption>
-                    <p>Lorem ipsum dolor sit amet conse...</p>
-                </figcaption>
-            </figure>
-        
-        </section>
+                <figure>
+                    <img src="./../assets/img/horsCourse/image3horscourses.png" alt="cycliste">
+                    <figcaption>
+                        <p>Lorem ipsum dolor sit amet conse...</p>
+                    </figcaption>
+                </figure>
+            
+            </section>
+        </div>
 
-        <section class="secondline">
-            <figure>
-                <img src="./../assets/img/home/image8accueil.png" alt="velo">
-                <figcaption>
-                    <p>Le vélo pours les nuls</p>
-                </figcaption>
-            </figure>
-        
-        </section>
+        <div class="content">
+            <section class="secondline">
+                <figure class="iconelinetwo">
+                    <img src="./../assets/img/home/image8accueil.png" alt="velo">
+                    <figcaption>
+                        <p>Le vélo pours les nuls</p>
+                    </figcaption>
+                </figure>
 
-        <section class="thirdline">
-            <figure>
-                <img src="./../assets/img/home/image9accueil.png" alt="roue">
-                <figcaption>
-                    <p>Tactique & technique</p>
-                </figcaption>
-            </figure>
-        </section>
+                <figure>
+                    <img src="./../assets/img/horsCourse/image4horscourses.png" alt="cycliste">
+                    <figcaption>
+                        <p>Lorem ipsum dolor sit amet conse...</p>
+                    </figcaption>
+                </figure>
+
+                <figure>
+                    <img src="./../assets/img/horsCourse/image5horscourses.png" alt="cycliste">
+                    <figcaption>
+                        <p>Lorem ipsum dolor sit amet conse...</p>
+                    </figcaption>
+                </figure>
+
+                <figure>
+                    <img src="./../assets/img/horsCourse/image6horscourses.png" alt="cycliste">
+                    <figcaption>
+                        <p>Lorem ipsum dolor sit amet conse...</p>
+                    </figcaption>
+                </figure>
+            
+            </section>
+        </div>
+
+        <div class="content">
+            <section class="thirdline">
+                <figure class="iconelinethree">
+                    <img src="./../assets/img/home/image9accueil.png" alt="roue">
+                    <figcaption>
+                        <p>Tactique & technique</p>
+                    </figcaption>
+                </figure>
+
+                <figure>
+                    <img src="./../assets/img/horsCourse/image7horscourses.png" alt="cycliste">
+                    <figcaption>
+                        <p>Lorem ipsum dolor sit amet conse...</p>
+                    </figcaption>
+                </figure>
+
+                <figure>
+                    <img src="./../assets/img/horsCourse/image8horscourses.png" alt="cycliste">
+                    <figcaption>
+                        <p>Lorem ipsum dolor sit amet conse...</p>
+                    </figcaption>
+                </figure>
+
+                <figure>
+                    <img src="./../assets/img/horsCourse/image9horscourses.png" alt="cycliste">
+                    <figcaption>
+                        <p>Lorem ipsum dolor sit amet conse...</p>
+                    </figcaption>
+                </figure>
+            </section>
+        </div>
     </div>
 
 </main>
