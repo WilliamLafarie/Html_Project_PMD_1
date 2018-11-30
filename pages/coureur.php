ef2c03a510e4575be2fcabb87638a181b92a8482
william <william.6napps@gmail.com>
2018-11-30 14:52:50 +0100

    Coureur 90%

--- pages/coureur.php
+++ pages/coureur.php
@@ -59,27 +59,36 @@
             <div class="fiche__perso">
                 <h2>Fiche d'identité</h2>
                 <div class="identite">
-                    <div class="identite__item">
-                        <p>Age</p>
-                        <p>Date d'anniversaire</p>
-                        <p>Lieu de naissance</p>
-                        <p>Taille</p>
-                        <p>Poids</p>
-                    </div>
-                    <div class="identite__valeur">
-                        <p>39 ans</p>
-                        <p>22 juin 1979</p>
-                        <p>Schiltigheim (France)</p>
-                        <p>1m77</p>
-                        <p>66kg</p>
-                    </div>
+                    <table class="identite__data" >
+                        <tr>
+                            <td>Age</td>
+                            <td>39 ans</td>
+                        </tr>
+                        <tr>
+                            <td>Date d'anniversaire</td>
+                            <td>22 juin 1979</td>
+                        </tr>
+                        <tr>
+                            <td>Lieu de naissance</td>
+                            <td>Schiltigheim (France)</td>
+                        </tr>
+                        <tr>
+                            <td>Taille</td>
+                            <td>1m77</td>
+                        </tr>
+                        <tr>
+                            <td>Poids</td>
+                            <td>66kg</td>
+                        </tr>
+                    </table>
                 </div>
                 <div class="palmares">
                     <h2>Palmarès</h2>
                      <div class="palmares__content">
-                     <div class="more">
-                            <p>2015</p>
-                        </div>
+
+                        <div class="palmares__date">
+                                <a>2015</a>
+                            </div>
 
                         <div class="palmares__text">
                             <img src="./../assets/img/home/next.svg" alt="arrow">
@@ -125,11 +134,54 @@
 
         </section>
 
-        </div>
 
-        
+        <section class="who">
+            <h2>Qui est Thomas ?</h2>
+            <article>
+                <p>Tantum autem cuique tribuendum, primum quantum ipse efficere possis, deinde etiam quantum ille quem diligas atque adiuves, sustinere. Non enim neque tu possis, quamvis excellas, omnes tuos ad honores amplissimos perducere, ut Scipio P. Rupilium potuit consulem efficere, fratrem eius L. non potuit. Quod si etiam possis quidvis deferre ad alterum, videndum est tamen, quid ille possit sustinere.</p>
 
+                <p>Coactique aliquotiens nostri pedites ad eos persequendos scandere clivos sublimes etiam si lapsantibus plantis fruticeta prensando vel dumos ad vertices venerint summos, inter arta tamen et invia nullas acies explicare permissi nec firmare nisu valido gressus: hoste discursatore rupium abscisa volvente, ruinis ponderum inmanium consternuntur, aut ex necessitate ultima fortiter dimicante, superati periculose per prona discedunt.</p>
+
+                <p>Quare hoc quidem praeceptum, cuiuscumque est, ad tollendam amicitiam valet; illud potius praecipiendum fuit, ut eam diligentiam adhiberemus in amicitiis comparandis, ut ne quando amare inciperemus eum, quem aliquando odisse possemus. Quin etiam si minus felices in diligendo fuissemus, ferendum id Scipio potius quam inimicitiarum tempus cogitandum putabat.</p>
+
+                <p>Quam ob rem ut ii qui superiores sunt submittere se debent in amicitia, sic quodam modo inferiores extollere. Sunt enim quidam qui molestas amicitias faciunt, cum ipsi se contemni putant; quod non fere contingit nisi iis qui etiam contemnendos se arbitrantur; qui hac opinione non modo verbis sed etiam opere levandi sunt.</p>
+            </article>
+        </section>
     </section>
+
+     <section class="actuCoureur">
+         <div class="content">
+
+            <h2>Actualité de Thomas</h2>
+            <section class="actuCoureur__news">
+
+                    <figure class="news__item">
+                        <img src="./../assets/img/home/image1accueil.png" alt="image1">
+                        <figcaption>Lorem ipsum dolor sit amet consectetur adipisicing elit.</figcaption>
+                    </figure>
+
+                    <figure class="news__item">
+                        <img src="./../assets/img/home/image2accueil.png" alt="image2">
+                        <figcaption>Lorem ipsum dolor sit amet consectetur adipisicing elit.</figcaption>
+                        
+                    </figure>
+
+                    <figure class="news__item">
+                        <img src="./../assets/img/home/image3accueil.png" alt="image3">
+                        <figcaption>Lorem ipsum dolor sit amet consectetur adipisicing elit.</figcaption>
+                    </figure>
+
+                    <figure class="news__item">
+                        <img src="./../assets/img/home/image4accueil.png" alt="image4">
+                        <figcaption>Lorem ipsum dolor sit amet consectetur adipisicing elit.</figcaption>
+                    </figure>
+            
+            </section>
+
+         </div>
+            
+    </section>
+
 
 </main>
 
