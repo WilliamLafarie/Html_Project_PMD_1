d1ed6e0037bc64db5b2a4fe4ad85488b479e76da
Melococoo <melody.akarawitage@hotmail.fr>
2018-11-30 15:12:38 +0100

    footer changement, presque fini

--- pages/include/footer.php
+++ pages/include/footer.php
@@ -1,125 +1,129 @@
-
 <section class="contact">
         <img src="./../assets/img/directenergielogo.png" alt="logo">
         <div class="slogan">
-            <p>1.5 millions de clients ont déjà choisi Direct Energie pour l'électricité et le gaz</p>
-            <p>Et vous, où en êtes-vous ? </p>
+                <p>1.5 millions de clients ont déjà choisi Direct Energie pour l'électricité et le gaz</p>
+                <p>Et vous, où en êtes-vous ? </p>
         </div>
         <div class="phone">
-            <p>Contactez-nous au </p>
-            <p>3099</p>
-            <p>Service et appel gratuit</p>
+                <p>Contactez-nous au </p>
+                <p>3099</p>
+                <p>Service et appel gratuit</p>
         </div>
 </section>
 
 <footer>
         <!-- -------------------------------- Premier footer ---------------------------------------- -->
 
-                <section id="footer1">
-                        <div class="content_footer">
-                                <div id="Xavier_1">
-                                        <img src="./../assets/img/imagefooter.png" class="imagefoot">
-                                        <article class="art_1">
-                                                <h4>Xavier Caïtucoli</h4>
-                                                <span class="PDG">PDG de Direct Energie</span>
-                                                <p class="para"> &laquo; Choisir le cyclisme à travers cette équipe
-                                                        chère aux
-                                                        français, est
-                                                        pour nous une évidence...</p>
-                                                <button class="lire_suite" type="button">lire la suite
-                                                </button>
-                                        </article>
-                                </div>
-
-                                <div id="Jean_Rene">
-                                        <img src="./../assets/img/imagefooter2.png" class="imagefoot">
-                                        <article class="art_1">
-                                                <h4>Jean-René Bernaudeau</h4>
-                                                <span class="PDG">Manager de Direct Energie Teams</span>
-                                                <p class="para">&laquo; J'ai choisi Direct Energie car je voulais un
-                                                        partenaire
-                                                        qui
-                                                        soit
-                                                        séduit par le travail de fond que je...</p>
-                                                <button class="lire_suite" type="button">lire la suite
-                                                </button>
-                                        </article>
-                                </div>
+        <section id="footer1">
+                <div class="content_footer">
+                        <div id="Xavier_1">
+                                <img src="./../assets/img/imagefooter.png" class="imagefoot">
+                                <article class="art_1">
+                                        <h4>Xavier Caïtucoli</h4>
+                                        <span class="PDG">PDG de Direct Energie</span>
+                                        <p class="para"> &laquo; Choisir le cyclisme à travers cette équipe
+                                                chère aux
+                                                français, est
+                                                pour nous une évidence...</p>
+                                        <button class="lire_suite" type="button">lire la suite
+                                        </button>
+                                </article>
                         </div>
 
-                </section>
+                        <div id="Jean_Rene">
+                                <img src="./../assets/img/imagefooter2.png" class="imagefoot">
+                                <article class="art_1">
+                                        <h4>Jean-René Bernaudeau</h4>
+                                        <span class="PDG">Manager de Direct Energie Teams</span>
+                                        <p class="para">&laquo; J'ai choisi Direct Energie car je voulais un
+                                                partenaire
+                                                qui
+                                                soit
+                                                séduit par le travail de fond que je...</p>
+                                        <button class="lire_suite" type="button">lire la suite
+                                        </button>
+                                </article>
+                        </div>
+                </div>
 
-<!-- ------------------------------------- Deuxième footer ------------------------------------------- -->
+        </section>
 
-                <section id="footer2">
+        <!-- ------------------------------------- Deuxième footer ------------------------------------------- -->
 
-                        <div class="content_footer2">
+        <section id="footer2">
 
-                                <section>
-                                        <h2>Newsletter</h2>
+                <div class="content_footer2">
 
-                                        <p>Recevez par mail les derniers résultats, les prochaines courses et les interviews de la Direct Energie Team</p>
+                        <section>
+                                <h2>Newsletter</h2>
 
-                                        <div class="bouton">
-                                                <input type="email" placeholder="votre email">
-                                                <input type="submit" value="Ok">
-                                        </div>
-                                        
-                                </section>
+                                <p>Recevez par mail les derniers résultats, les prochaines courses et les interviews de
+                                        la Direct Energie Team</p>
 
-                                <section>
-                                        <h2>Réseaux sociaux</h2>
-                                        <div class="reseau">
-                                                
-                                                <figure>
-                                                
-                                                        <img src="./../assets/img/youtube.png" alt="youtube">
-                                                        
-                                                        <figcaption>youtube</figcaption>
-                                                </figure>
+                                <div class="bouton">
+                                        <input type="email" placeholder="votre email">
+                                        <input type="submit" value="Ok">
+                                </div>
 
-                                                <figure>
+                        </section>
+
+                        <section>
+                                <h2>Réseaux sociaux</h2>
+                                <div class="reseau">
+                                        <figure>
+
+                                                <img src="./../assets/img/youtube.png" alt="youtube">
+
+                                                <figcaption>youtube</figcaption>
+                                        </figure>
+
+                                        <figure>
                                                 <img src="./../assets/img/twitter.png" alt="twitter">
-                                                        <figcaption>twitter</figcaption>
-                                                </figure>
+                                                <figcaption>twitter</figcaption>
+                                        </figure>
 
-                                                <figure>
-                                                        <img src="./../assets/img/insta.png" alt="instagram">
-                                                        <figcaption>instagram</figcaption>
-                                                </figure>
+                                        <figure>
+                                                <img src="./../assets/img/insta.png" alt="instagram">
+                                                <figcaption>instagram</figcaption>
+                                        </figure>
 
-                                                <figure>
-                                                        <img src="./../assets/img/fb.png" alt="facebook">
-                                                        <figcaption>facebook</figcaption>
-                                                </figure>
+                                        <figure class="fb">
+                                                <img src="./../assets/img/fb.png" alt="facebook">
+                                                <figcaption>facebook</figcaption>
+                                        </figure>
 
-                                                <figure>
-                                                        <img src="./../assets/img/google.png" alt="google+">
-                                                        <figcaption>google+</figcaption>
-                                                </figure>
-                                        </div>
-                                </section>
+                                        <figure class="gg">
+                                                <img src="./../assets/img/google.png" alt="google+">
+                                                <figcaption>google+</figcaption>
+                                        </figure>
+                                </div>
+                        </section>
 
-                                <section>
-                                        <h2>Kit de presse</h2>
-                                        <p>Télécharger le dossier de presse: Photo des joueurs, communiqué de presse...</p>
+                        <section>
+                                <h2>Kit de presse</h2>
+                                <p>Télécharger le dossier de presse: Photo des joueurs, communiqué de presse...</p>
 
-                                        <div class="pdf">
-                                                <img src="" alt="pdf">
-                                                <a href="">Télécharger le dossier (33mo)</a>
-                                        </div>
-                                </section>
+                                <div class="pdf">
+                                        <img src="./../assets/img/Bitmap1.png" id="dossier" alt="pdf">
+                                        <a href="" id="doss">Télécharger le dossier (33mo)</a>
+                                </div>
+                        </section>
 
-                        </div>
+                </div>
 
-                        <p class="mentions">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        </p>
+                <p class="mentions">
+                        <div id="mentions">
+                        <a href="" id="f1"> &copy; Direct energie Team 2016 Tous Droits Réservés </a>
+                        <a href="" id="ff">Mentions Légales </a>
+                        <a href="" id="ff">Création du site internet </a>
+</div>
+                </p>
 
-                </section>
+        </section>
 
 
 </footer>
 
 </body>
+
 </html>
